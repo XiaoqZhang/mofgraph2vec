3ca285907b360c4d3b6124c969a1031e584d2e6c
XiaoqZhang <xiaoqi.zhang@epfl.ch>
2023-01-27 18:43:10 +0100

    debug check the doc2vec model

--- src/mofgraph2vec/model/run.py
+++ src/mofgraph2vec/model/run.py
@@ -1,6 +1,8 @@
 import os
 from glob import glob
 from tqdm import tqdm
+from loguru import logger
+from collections import Counter
 import pandas as pd
 from mofgraph2vec.graph.cif2graph import MOFDataset
 from mofgraph2vec.graph.mof2vec import WeisfeilerLehmanMachine
@@ -14,30 +16,38 @@ def main():
     :param args: Object with the arguments.
     """
     cifs = glob("../../../data/cifs/*.cif")
-    print("\nFeature extraction started.\n")
+    logger.info(f"Feature extraction started.")
     dataset_loader = MOFDataset(strategy="vesta")
 
+    corpus = []
     documents = []
     graphs = []
     for cif in tqdm(cifs):
         name = cif.split("/")[-1].rstrip(".cif")
         graph, feature = dataset_loader.to_WL_machine(cif)
         machine = WeisfeilerLehmanMachine(graph, feature, 4)
-        doc = TaggedDocument(words=machine.extracted_features, tags=["g_" + name])
+        word = machine.extracted_features
+        doc = TaggedDocument(words=word, tags=["g_" + name])    # to check: what does label do? 
+        corpus.append(word)
         documents.append(doc)
         graphs.append(graph)
+    
+    corpus = [word for words in corpus for word in words]
+    distribution = Counter(corpus)
 
-    print("\nOptimization started.\n")
-
-    model = Doc2Vec(documents,
-                    vector_size=64,
+    logger.info(f"Inistantiate model.")
+    model = Doc2Vec(vector_size=64,
                     window=10,
-                    min_count=4,
+                    min_count=3,
                     dm=0,
                     sample=0.9,
-                    workers=1,
+                    workers=4,
                     epochs=10,
                     alpha=0.001)
+    model.build_vocab(documents)
+    logger.info(f"Start training.")
+    model.train(documents, total_examples=model.corpus_count, epochs=40)
+
 
     save_embedding("../../../data/vec/embedding.csv", model, cifs, 64)
 
