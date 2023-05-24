719776e47e3b9fe0eb58f6edbc7070441b507607
XiaoqZhang <xiaoqi.zhang@epfl.ch>
2023-05-24 10:36:15 +0200

    remove debug information

--- src/mofgraph2vec/featurize/tokenize.py
+++ src/mofgraph2vec/featurize/tokenize.py
@@ -48,8 +48,7 @@ class WeisfeilerLehmanMachine:
         
         feature_storage = {}
         new_features = {}
-        logger.debug(f"Node indices: {self.nodes_idx}")
-        logger.debug(f"Linker indices: {self.linker_idx}")
+
         for node in self.nodes:
             feature_storage = get_new_features(feature_storage, node)
             if self.mode == "all":
